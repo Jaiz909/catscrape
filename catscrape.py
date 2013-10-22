2c47581e0341b6bf8bd36be309c548328e759918
Jai Grimshaw <jai@jaigrimshaw.com>
2013-10-23 03:28:28 +1100

    Swapped to BeautifulSoup. Cleaned code.
    Originally wrote catscrape in the middle of the night. Cleaned up stupid mistakes.
    Switched to parsing with BeautifulSoup. The regex was always a hack.

--- catscrape.py
+++ catscrape.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 Author: Jai Grimshaw <jai@jaigrimshaw.com>
 Date: 20130724
@@ -13,6 +13,7 @@ import os
 import os.path
 import argparse
 import logging
+from bs4 import BeautifulSoup
 
 logger = logging.getLogger('catscrape')
 
@@ -23,6 +24,8 @@ class ImgurDownloader:
 		self.numThreads = numThreads
 
 		self.albumPath = savePath
+
+                self.imgurURLRegex = re.compile('(http\:\/\/)?(\/\/)?(?P<fullURL>i\.imgur\.com\/(?P<filename>(?P<imageID>[a-zA-Z0-9]+)\.(?P<ext>jpg|jpeg|png|gif)))')
 
 		#Prep threads. Start them after populating self.images
 		for i in range(0, self.numThreads):
@@ -52,23 +55,36 @@ class ImgurDownloader:
 		album = urllib2.urlopen('http://imgur.com/a/%s/noscript' % albumId)
                 logger.info('Downloaded HTML for album: \'%s\'', albumId)
 		albumHTML = album.read()
-		#NEVER USE REGEX TO PARSE HTML. THIS WILL SUMMON LUCIFER HIMSELF.
+                soup = BeautifulSoup(albumHTML)
                 logger.info('Searching for images.')
-                imageMatches = set()
-                imageMatchList = list()
-		imageMatchList += re.findall('<img src="(?P<fullURL>http\:\/\/i\.imgur\.com\/(?P<filename>(?P<imageID>[a-zA-Z0-9]+)\.(?P<ext>jpg|jpeg|png|gif)))', albumHTML)
-		imageMatchList = re.findall('<img src="\/\/(?P<fullURL>i\.imgur\.com\/(?P<filename>(?P<imageID>[a-zA-Z0-9]+)\.(?P<ext>jpg|jpeg|png|gif)))', albumHTML)
-                for match in imageMatchList:
-                        imageMatches.add(match)
-                for match in imageMatches:
-                        logger.info('Found: %s' % (str(match)))
+
+                imageList = list() #Contains img tags
+                #Images can be too small to place in a zoom div. All interesting images are in wrappers.
+                imageWrappers = soup.find_all('div', class_='wrapper')
+                for wrapper in imageWrappers:
+                        image = wrapper.find('img')
+                        if image is not None:
+                                imageList.append(image)
+
+                imageMatches = list() #Contains re.match objects
+                
+                for image in imageList:
+                        imageLink = image.get('src', None)
+                        if imageLink is None:
+                                logger.warning('Image \'%s\' does not contain a src attribute. Skipping image.', str(image))
+                                return
+
+                        match = re.match(self.imgurURLRegex, imageLink)
+                        imageMatches.append(match)
+
 		#Use a count to preserve the order of the album.
 		count = 0
 		for match in imageMatches:
-                        url = match[0]
+                        url = match.group('fullURL')
                         if url.startswith('http://') == False and url.startswith('https://') == False:
                                 url = 'http://' + url
-			self.images.append({'location':count, 'url':url, 'filename':match[1], 'id':match[2], 'extension':match[3]})
+                        logger.info('Found: %s' % (str(url)))
+			self.images.append({'location':count, 'url':url, 'filename':match.group('filename'), 'id':match.group('imageID'), 'extension':match.group('ext')})
 			count = count + 1
 
 
