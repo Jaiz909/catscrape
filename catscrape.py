c21249b136abc3b71bc6b5d1b6f77a6c4d199d76
Jai Grimshaw <jai@jaigrimshaw.com>
2013-10-23 05:29:38 +1100

    Retabbed catscrape

--- catscrape.py
+++ catscrape.py
@@ -18,45 +18,45 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger('catscrape')
 
 class ImgurDownloader:
-	def __init__(self, savePath='.', numThreads=10):
-		self.images = list()
-		self.threads = list()
-		self.numThreads = numThreads
+        def __init__(self, savePath='.', numThreads=10):
+                self.images = list()
+                self.threads = list()
+                self.numThreads = numThreads
 
-		self.albumPath = savePath
+                self.albumPath = savePath
 
                 self.imgurURLRegex = re.compile('(http\:\/\/)?(\/\/)?(?P<fullURL>i\.imgur\.com\/(?P<filename>(?P<imageID>[a-zA-Z0-9]+)\.(?P<ext>jpg|jpeg|png|gif)))')
 
-		#Prep threads. Start them after populating self.images
-		for i in range(0, self.numThreads):
+                #Prep threads. Start them after populating self.images
+                for i in range(0, self.numThreads):
                         logger.debug('Prepping thread %d.', i)
-			thread = threading.Thread(target=self.downloadImage)
-			self.threads.append(thread)
+                        thread = threading.Thread(target=self.downloadImage)
+                        self.threads.append(thread)
 
-	def downloadAlbum(self, url):
-		match = re.match('http\:\/\/(www\.)?imgur\.com/a/([a-zA-Z0-9]+)(#[0-9]+)?', url)
+        def downloadAlbum(self, url):
+                match = re.match('http\:\/\/(www\.)?imgur\.com/a/([a-zA-Z0-9]+)(#[0-9]+)?', url)
                 if match is None:
                         logger.error('URL does not match expected URL pattern.')
                         return None
-		self.albumPath = os.path.abspath(os.path.join(self.albumPath, match.group(2)))
+                self.albumPath = os.path.abspath(os.path.join(self.albumPath, match.group(2)))
                 try:
                         os.mkdir(self.albumPath)
                 except OSError as e:
                         logger.warning('Exception thrown when creating album directory: \'%s\'', str(e))
-		self.enumerateAlbum(match.group(2))
+                self.enumerateAlbum(match.group(2))
 
-		for thread in self.threads:
-			thread.start()
+                for thread in self.threads:
+                        thread.start()
                 for thread in self.threads:
                         thread.join()
 
 
-	def enumerateAlbum(self, albumId):
+        def enumerateAlbum(self, albumId):
                 logger.info('Enumerating album.')
-		#Grab the noscript version of the URL
-		album = urllib2.urlopen('http://imgur.com/a/%s/noscript' % albumId)
+                #Grab the noscript version of the URL
+                album = urllib2.urlopen('http://imgur.com/a/%s/noscript' % albumId)
                 logger.info('Downloaded HTML for album: \'%s\'', albumId)
-		albumHTML = album.read()
+                albumHTML = album.read()
                 soup = BeautifulSoup(albumHTML)
                 logger.info('Searching for images.')
 
@@ -79,38 +79,38 @@ class ImgurDownloader:
                         match = re.match(self.imgurURLRegex, imageLink)
                         imageMatches.append(match)
 
-		#Use a count to preserve the order of the album.
-		count = 0
-		for match in imageMatches:
+                #Use a count to preserve the order of the album.
+                count = 0
+                for match in imageMatches:
                         url = match.group('fullURL')
                         if url.startswith('http://') == False and url.startswith('https://') == False:
                                 url = 'http://' + url
                         logger.info('Found: %s' % (str(url)))
-			self.images.append({'location':count, 'url':url, 'filename':match.group('filename'), 'id':match.group('imageID'), 'extension':match.group('ext')})
-			count = count + 1
+                        self.images.append({'location':count, 'url':url, 'filename':match.group('filename'), 'id':match.group('imageID'), 'extension':match.group('ext')})
+                        count = count + 1
 
 
-	def downloadImage(self):
-		#grab an image from the list until we can't.
-		try:
-			while True:
-				image = self.images.pop()
-				imageLoc = image['location']
-				imageURL = image['url']
+        def downloadImage(self):
+                #grab an image from the list until we can't.
+                try:
+                        while True:
+                                image = self.images.pop()
+                                imageLoc = image['location']
+                                imageURL = image['url']
                                 logger.info('Downloading: %s' % (imageURL))
-				imageFilename = image['filename']
-				urllib.urlretrieve(imageURL, os.path.join(self.albumPath, '%d_%s' % (imageLoc, imageFilename)))
-		except IndexError:
-			return
+                                imageFilename = image['filename']
+                                urllib.urlretrieve(imageURL, os.path.join(self.albumPath, '%d_%s' % (imageLoc, imageFilename)))
+                except IndexError:
+                        return
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
+        parser = argparse.ArgumentParser()
         parser.add_argument('-d', dest='debug', action='store_true', help='Enable debugging output', default=False, required=False)
         parser.add_argument('-q', dest='quiet', action='store_true', help='Enable quiet mode', default=False, required=False)
-	parser.add_argument('-a', dest='album', help='The imgur album url to download images from.', required=True)
-	parser.add_argument('-o', dest='outdir', help='The directory to save images to. Defaults to the album id in the current working directory.', default='.', required=False)
-	parser.add_argument('-n', dest='numthreads', help='The number of threads to use for downloading. Default=10.', type=int, default=10, required=False)
-	args = parser.parse_args()
+        parser.add_argument('-a', dest='album', help='The imgur album url to download images from.', required=True)
+        parser.add_argument('-o', dest='outdir', help='The directory to save images to. Defaults to the album id in the current working directory.', default='.', required=False)
+        parser.add_argument('-n', dest='numthreads', help='The number of threads to use for downloading. Default=10.', type=int, default=10, required=False)
+        args = parser.parse_args()
         
         logger.addHandler(logging.StreamHandler())
         if args.debug:
@@ -122,6 +122,6 @@ if __name__ == '__main__':
 
 
         logger.debug('Constructing downloader.')
-	downloader = ImgurDownloader(savePath=args.outdir, numThreads=args.numthreads)
+        downloader = ImgurDownloader(savePath=args.outdir, numThreads=args.numthreads)
         logger.debug('Starting downloader.')
-	downloader.downloadAlbum(url=args.album)
+        downloader.downloadAlbum(url=args.album)
