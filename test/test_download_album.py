e2128bc605c7b20e6b00c8fbd1ac8c924ddacc7d
Jai Grimshaw <jai@jaigrimshaw.com>
2013-10-23 04:38:57 +1100

    Improved quality of unit test

--- test/test_download_album.py
+++ test/test_download_album.py
@@ -16,6 +16,7 @@ class TestAlbumDownload(unittest.TestCase):
                 self.expectedFiles.append({'url':'http://i.imgur.com/dHO0Lzz.jpg', 'filename':'1_dHO0Lzz.jpg', 'md5':'5cf89cfab1174a62cc095a396fcf09bf'})
                 self.expectedFiles.append({'url':'http://i.imgur.com/UsFSHdD.jpg', 'filename':'2_UsFSHdD.jpg', 'md5':'f815af133e66b9c6a5ea7073ea9af52a'})
 
+
         def testDiscovery(self):
                 i = catscrape.ImgurDownloader(savePath=tempfile.mkdtemp())
                 i.enumerateAlbum(albumId=self.albumID)
@@ -27,22 +28,35 @@ class TestAlbumDownload(unittest.TestCase):
 
                 for e in notFound:
                         self.assertIn(e['url'], foundURLs)
+                        foundURLs.remove(e['url'])
+                self.assertEqual(len(foundURLs), 0)
 
 
-        def testFiles(self):
+        def testDownload(self):
                 downloadDir = tempfile.mkdtemp()
                 i = catscrape.ImgurDownloader(savePath=downloadDir)
                 i.downloadAlbum(url=self.albumURL)
 
                 albumPath = os.path.join(downloadDir, self.albumID)
                 downloadedFiles = os.listdir(albumPath)
-                notFound = self.expectedFiles
+                notFound = list()
+                for e in self.expectedFiles:
+                        notFound.append(e['filename'])
+ 
                 for e in self.expectedFiles:
                         self.assertIn(e['filename'], downloadedFiles)
                         with open(os.path.join(albumPath, e['filename']), 'rb') as f:
                                 h = hashlib.md5()
                                 h.update(f.read())
-                                self.assertEqual(h.hexdigest(), e['md5'])
+                                self.assertEqual(h.hexdigest(), e['md5'], msg='File hash and expected hash do not match.')
+                        #Keep track of what we expect to see and haven't, and what we have but didn't expect to see.
+                        notFound.remove(e['filename'])
+                        downloadedFiles.remove(e['filename'])
+
+                #Should have seen all files, and there should be no files we didn't expect
+                self.assertEqual(len(notFound), 0, msg='Not all expected files were downloaded.')
+                self.assertEqual(len(downloadedFiles), 0, msg='Too many files were downloaded.')
+
 
 if __name__ == '__main__':
         unittest.main()
